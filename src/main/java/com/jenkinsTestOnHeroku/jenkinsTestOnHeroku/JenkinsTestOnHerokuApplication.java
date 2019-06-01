e21dfe09edd78ff920801dee768acc45e42ad2ec
unknown <modi.yash2012@gmail.com>
2019-06-01 14:06:21 -0300

    Initialize Project

---
+++ src/main/java/com/jenkinsTestOnHeroku/jenkinsTestOnHeroku/JenkinsTestOnHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsTestOnHeroku.jenkinsTestOnHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestOnHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestOnHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsTestOnHeroku/jenkinsTestOnHeroku/JenkinsTestOnHerokuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkinsTestOnHeroku.jenkinsTestOnHeroku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsTestOnHerokuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
